8ff516ab012f077909c101523da2f3775889042e
Faisal <faisalhmohd@gmail.com>
2020-01-31 05:34:26 +0600

    Add Service List basic structure and hover animation for menu items

--- src/NavBar/NavMenu.jsx
+++ src/NavBar/NavMenu.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function MenuItems() {
+function MenuItems({ hoveredItem, setHoveredItem }) {
   const items = [
     'Helias',
     'Hoboken Yogi',
@@ -11,22 +11,55 @@ function MenuItems() {
 
   return (
     <ul className="nv-NavMenu-items">
-      {items.map(item => (
-        <React.Fragment>
-          <li>
-            <a>{ item }</a>
-          </li>
-          <br />
-        </React.Fragment>
-      ))}
+      {items.map(item => {
+        const isAnyItemHovered = hoveredItem;
+        const isCurrentItemHovered = hoveredItem && hoveredItem === item;
+
+        let itemClassName = '';
+        if (isCurrentItemHovered) {
+          itemClassName = 'hovered';
+        }
+        else if (isAnyItemHovered) {
+          itemClassName = 'faded';
+        }
+
+        return (
+          <React.Fragment key={item}>
+            <li>
+              <a
+                className={itemClassName}
+                onMouseEnter={() => setHoveredItem(item)}
+                onMouseLeave={() => setHoveredItem(false)}
+              >
+                { item }
+              </a>
+            </li>
+            <br />
+          </React.Fragment>
+        );
+      })}
     </ul>
   );
 }
 
+function ServiceList() {
+  return (
+    <div className="nv-NavMenu-ServiceList">
+      <div className="nv-NavMenu-ServiceList-default">
+        <span>
+          Service
+        </span>
+      </div>
+    </div>
+  );
+}
+
 function NavMenu({ isMenuVisible }) {
+  const [ hoveredItem, setHoveredItem ] = useState(false);
   return (
     <div className={`nv-NavMenu ${ isMenuVisible && 'visible' }`}>
-      <MenuItems />
+      <MenuItems hoveredItem={hoveredItem} setHoveredItem={setHoveredItem} />
+      <ServiceList />
     </div>
   );
 }
