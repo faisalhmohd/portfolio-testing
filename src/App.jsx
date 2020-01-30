f13c83498f83d4ab3893f3e0a18277377cb3adcf
Faisal <faisalhmohd@gmail.com>
2020-01-31 00:35:30 +0600

    Move JS to JSX

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function NavBar() {
+  return (
+    <div className="nv">
+      <div>Hamburger menu</div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <NavBar />
+  );
+}
+
+export default App;
