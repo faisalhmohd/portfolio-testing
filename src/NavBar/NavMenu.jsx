34b5f1bbb9539bef87498597e9e6eafcef96303d
Faisal <faisalhmohd@gmail.com>
2020-01-31 05:11:03 +0600

    Componentize MenuItems

--- src/NavBar/NavMenu.jsx
+++ src/NavBar/NavMenu.jsx
@@ -1,39 +1,45 @@
 import React from 'react';
 
+function MenuItems() {
+  return (
+    <ul className="nv-NavMenu-items">
+      <li>
+        <a>
+          Helias
+        </a>
+      </li>
+      <br />
+      <li>
+        <a>
+          Hoboken Yogi
+        </a>
+      </li>
+      <br />
+      <li>
+        <a>
+          Buzzworthy
+        </a>
+      </li>
+      <br />
+      <li>
+        <a>
+          Gatto
+        </a>
+      </li>
+      <br />
+      <li>
+        <a>
+          Snooze
+        </a>
+      </li>
+    </ul>
+  );
+}
+
 function NavMenu({ isMenuVisible }) {
   return (
     <div className={`nv-NavMenu ${ isMenuVisible && 'visible' }`}>
-      <ul className="nv-NavMenu-items">
-        <li>
-          <a>
-            Helias
-          </a>
-        </li>
-        <br />
-        <li>
-          <a>
-            Hoboken Yogi
-          </a>
-        </li>
-        <br />
-        <li>
-          <a>
-            Buzzworthy
-          </a>
-        </li>
-        <br />
-        <li>
-          <a>
-            Gatto
-          </a>
-        </li>
-        <br />
-        <li>
-          <a>
-            Snooze
-          </a>
-        </li>
-      </ul>
+      <MenuItems />
     </div>
   );
 }
