41780263f323ea017c1318683a73e5caf6f3c832
Faisal <faisalhmohd@gmail.com>
2020-01-31 07:16:56 +0600

    Add Close and All Projects text in Menu button

--- src/NavBar/NavMenuButton.jsx
+++ src/NavBar/NavMenuButton.jsx
@@ -15,18 +15,28 @@ const dotPositions = {
 
 function NavMenuButton({ isMenuVisible, setMenuVisible }) {
   return (
-    <div className={`nv-NavMenuButton ${ isMenuVisible && 'visible' }`} onClick={() => setMenuVisible(!isMenuVisible)}>
-      <span style={{ transform: `translate(${ dotPositions.column.one }, ${ dotPositions.row.one })` }}></span>
-      <span style={{ transform: `translate(${ dotPositions.column.two }, ${ dotPositions.row.one })` }}></span>
-      <span style={{ transform: `translate(${ dotPositions.column.three }, ${ dotPositions.row.one })` }}></span>
+    <div className={`nv-NavMenuButton ${ isMenuVisible && 'visible' }`}>
+      <div className="nv-NavMenuButton-label">
+        <span>
+          All Projects
+        </span>
+        <span>
+          Close
+        </span>
+      </div>
+      <div className="nv-NavMenuButton-icon" onClick={() => setMenuVisible(!isMenuVisible)}>
+        <span style={{ transform: `translate(${ dotPositions.column.one }, ${ dotPositions.row.one })` }}></span>
+        <span style={{ transform: `translate(${ dotPositions.column.two }, ${ dotPositions.row.one })` }}></span>
+        <span style={{ transform: `translate(${ dotPositions.column.three }, ${ dotPositions.row.one })` }}></span>
 
-      <span style={{ transform: `translate(${ dotPositions.column.one }, ${ dotPositions.row.two })` }}></span>
-      <span style={{ transform: `translate(${ dotPositions.column.two }, ${ dotPositions.row.two })` }}></span>
-      <span style={{ transform: `translate(${ dotPositions.column.three }, ${ dotPositions.row.two })` }}></span>
+        <span style={{ transform: `translate(${ dotPositions.column.one }, ${ dotPositions.row.two })` }}></span>
+        <span style={{ transform: `translate(${ dotPositions.column.two }, ${ dotPositions.row.two })` }}></span>
+        <span style={{ transform: `translate(${ dotPositions.column.three }, ${ dotPositions.row.two })` }}></span>
 
-      <span style={{ transform: `translate(${ dotPositions.column.one }, ${ dotPositions.row.three })` }}></span>
-      <span style={{ transform: `translate(${ dotPositions.column.two }, ${ dotPositions.row.three })` }}></span>
-      <span style={{ transform: `translate(${ dotPositions.column.three }, ${ dotPositions.row.three })` }}></span>
+        <span style={{ transform: `translate(${ dotPositions.column.one }, ${ dotPositions.row.three })` }}></span>
+        <span style={{ transform: `translate(${ dotPositions.column.two }, ${ dotPositions.row.three })` }}></span>
+        <span style={{ transform: `translate(${ dotPositions.column.three }, ${ dotPositions.row.three })` }}></span>
+      </div>
     </div>
   );
 }
