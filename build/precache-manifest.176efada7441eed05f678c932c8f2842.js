545ab8242796ad6c00b1b70ad0c2be6385e19ef9
Faisal <faisalhmohd@gmail.com>
2020-02-01 00:36:36 +0600

    Add build files

---
+++ build/precache-manifest.176efada7441eed05f678c932c8f2842.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e2428476570c36934fcc457cd8eec1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f9d16f970c8cb1cd2f7",
+    "url": "/static/css/main.562adbf1.chunk.css"
+  },
+  {
+    "revision": "29ba6a5161fc628e56be",
+    "url": "/static/js/2.8cef289f.chunk.js"
+  },
+  {
+    "revision": "5f9d16f970c8cb1cd2f7",
+    "url": "/static/js/main.a127ac4b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
